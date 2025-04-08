aa78939b27a1cf037bb62d3bd7678033325fab18
LittleMega777 <pierrecouto02@gmail.com>
2025-04-08 11:21:41 -0300

    feat(main.py) - codigo bruto de info_corridas_do_dia

--- ExercicioCsv/src/main.py
+++ ExercicioCsv/src/main.py
@@ -1,13 +1,27 @@
 import pandas as pd
 
-df_info_transportes = pd.read_csv('info_transportes.csv',
-                 sep=';',
-                 encoding='utf-8',
-                 )
-
-df_info_transportes['DATA_INICIO'] = pd.to_datetime(df_info_transportes['DATA_INICIO']).dt.strftime('%Y-%m-%d')
-# df_info_transportes_corridos = df_info_transportes['DATA_INICIO']
 
 
 
-print(df_info_transportes[['DATA_INICIO', 'LOCAL_INICIO', 'LOCAL_FIM', 'DISTANCIA', 'PROPOSITO']].head(10))
+
+if __name__ == "__main__":
+    
+    df_info_transportes = pd.read_csv('info_transportes.csv',
+                    sep=';',
+                    encoding='utf-8',
+                    )
+
+    df_info_transportes['DT_REFE'] = pd.to_datetime(df_info_transportes['DATA_INICIO'], format='%m-%d-%Y %H:%M').dt.strftime('%Y-%m-%d')
+
+    df_info_corridas_do_dia = df_info_transportes.groupby('DT_REFE').agg(
+        QT_CORR=('DATA_INICIO', 'count'),
+        QT_CORR_NEG=('CATEGORIA', lambda x: (x == 'Negocio').sum()),
+        QT_CORR_PESS=('CATEGORIA', lambda x: (x == 'Pessoal').sum()),
+        VL_MAX_DIST=('DISTANCIA', 'max'),
+        VL_MIN_DIST=('DISTANCIA', 'min'),
+        VL_AVG_DIST=('DISTANCIA', 'mean'),
+        QT_CORR_REUNI=('PROPOSITO', lambda x: (x == 'Reunião').sum()),
+        QT_CORR_NAO_REUNI=('PROPOSITO', lambda x: ((x.notna()) & (x != 'Reunião')).sum())
+    ).reset_index()
+
+    print(df_info_corridas_do_dia.head(10))
